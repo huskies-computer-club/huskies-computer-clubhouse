ed6dcc75cb268c4bbbb63874f739ff80ad28cbb3
Luis Martinez <mrluismartinezzz@gmail.com>
2024-03-05 05:09:19 -0800

    adds background-beams to home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,6 @@
 import { auth, clerkClient } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
-import { cn } from "@/utils/cn";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 
 export default async function DashboardPage() {
@@ -22,11 +20,12 @@ export default async function DashboardPage() {
                         👋 Hi, {user.firstName || `Stranger`}
                     </h1>
                     <h2 className="mt-16 mb-4 text-3xl font-semibold">
-                       You have been added to the raffle! 🎟🎫🎰💸 
+                        You have been added to the raffle! 🎟🎫🎰💸
                     </h2>
                     <p>The winner will receive an email to pick up the price.</p>
                 </>
             )}
+            <BackgroundBeams />
         </>
     );
 }
